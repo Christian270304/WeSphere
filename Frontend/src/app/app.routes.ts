9f1194b0ff3e50b74070aa732f4c0be88d57bf4e
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-04-01 16:00:19 +0200

    add messages route with auth guard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './features/home/home.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ProfileComponent } from './features/profile/profile.component';
 import { AuthComponent } from './features/auth/auth.component';
+import { MessagesComponent } from './features/messages/messages.component';
 
 export const routes: Routes = [
     { path: '', component: AuthComponent },
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: 'home' }
 ];
