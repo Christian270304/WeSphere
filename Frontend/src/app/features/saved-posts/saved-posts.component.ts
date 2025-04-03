834a8bd71694173309c20941e62b7e692f847715
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-04-03 22:59:26 +0200

    add saved posts component with styles and tests

---
+++ Frontend/src/app/features/saved-posts/saved-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-saved-posts',
+  imports: [RouterModule],
+  templateUrl: './saved-posts.component.html',
+  styleUrl: './saved-posts.component.scss'
+})
+export class SavedPostsComponent {
+
+}
