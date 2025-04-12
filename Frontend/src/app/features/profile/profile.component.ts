8932cd72720e8fdb72c836e51241dd8b4a16851e
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-04-13 01:46:37 +0200

    feat: enhance profile component to handle user retrieval by username and display error message for non-existent users

--- Frontend/src/app/features/profile/profile.component.ts
+++ Frontend/src/app/features/profile/profile.component.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { UserStatsComponent } from '../../shared/components/user-stats/user-stats.component';
 import { UserActionsComponent } from '../../shared/components/user-actions/user-actions.component';
-import { HttpClient } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PostsComponent } from '../../shared/components/posts/posts.component';
 import { HeaderStateService } from '../../core/services/header-state.service';
 import { UserService } from '../../core/services/user.service';
@@ -16,18 +15,48 @@ import { UserService } from '../../core/services/user.service';
 })
 export class ProfileComponent {
   public user: any = {};
+  public userId: number | null = null;
+  public isOwnProfile: boolean = false;
+  public noExists: boolean = false;
+  public username: string | null = null;
 
-  constructor(private http: HttpClient, private headerStateService: HeaderStateService, private userService: UserService) {}
+  constructor(private route: ActivatedRoute, private headerStateService: HeaderStateService, private userService: UserService,  private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-   
     this.headerStateService.setHideElements(true);
-    
-    this.userService.getUser().subscribe((user) => {
-      this.user = user;
-      console.log('User data:', this.user);
+
+    this.route.paramMap.subscribe(params => {
+      const usernameFromUrl = params.get('username');
+      this.userId = null;  
+
+      if (!usernameFromUrl) {
+        this.userService.getUser().subscribe(user => {
+          this.user = user;
+          this.userId = user.id;
+          this.isOwnProfile = true;
+          this.noExists = false; 
+          this.cdr.detectChanges(); 
+        });
+      } else {
+        this.userService.getUserByUsername(usernameFromUrl).subscribe((profileUser) => {
+          if (profileUser) {
+            this.user = profileUser.user;
+            this.userId = profileUser.user.id;
+            this.isOwnProfile = profileUser.current_user_id === profileUser.user.id;
+            this.noExists = false; 
+            this.cdr.detectChanges(); 
+          } else {
+            this.noExists = true;  
+            this.cdr.detectChanges(); 
+          }
+        });
+      }
     });
-    
   }
 
+  ngOnDestroy() {
+    this.userId = null;
+  }
+  
+  
 }
