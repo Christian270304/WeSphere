32bd3a16fedd87a39d82e1247cb0dcc5453e70a1
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-05-07 18:41:51 +0200

    feat: refactor post service to improve error handling and data management for posts, users, and saved posts

--- Frontend/src/app/core/services/post.service.ts
+++ Frontend/src/app/core/services/post.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -10,74 +10,79 @@ import { environment } from '../../../environments/environment';
 export class PostService {
   private apiUrl = environment.apiUrl;
   private postsSubject = new BehaviorSubject<any>(null);
+  private postsUserSubject = new BehaviorSubject<any>(null);
+  private postsSavedSubject = new BehaviorSubject<any>(null);
   posts$ = this.postsSubject.asObservable();
+  postsUser$ = this.postsUserSubject.asObservable();
+  postsSaved$ = this.postsSavedSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  getPosts(userArticles = false, userId: number | null = null, saved = false, limit: number = 20, offset: number = 0) {
-    if (userArticles) {
-      this.http.get<any>(`${this.apiUrl}/posts/user/${userId}`, {withCredentials: true}).subscribe(
-        (data) => {
-          console.log("Posts obtenidos:", data);
-          if (Array.isArray(data.posts)) {
-            this.postsSubject.next([]);
-            this.postsSubject.next(data.posts);
-          } else {
-            console.error("Error: La API no devolvió un array", data);
-            this.postsSubject.next([]);
-          }
-        },
-        (error) => {
-          console.error("Error al obtener posts:", error);
-          this.postsSubject.next([]);
-        }
-      );
-      return this.posts$;
-    } else if (saved) {
-      this.postsSubject.next([]);
-
-      this.http.get<any>(`${this.apiUrl}/posts/savedposts`, {withCredentials: true}).subscribe(
-        (data) => {
-          this.postsSubject.next([]);
-          if (Array.isArray(data.posts)) {
-            if (data.posts.length === 0) {
-              console.log("No hay publicaciones guardadas");
-              this.postsSubject.next([]); 
-            } else {
-              
-              this.postsSubject.next(data.posts); // Emitir las publicaciones
-            }
-          } else {
-            console.error("Error: La API no devolvió un array", data);
-            this.postsSubject.next([]); // Emitir un array vacío en caso de error
-          }
-          
-        },
-        (error) => {
-          console.error("Error al obtener posts:", error);
-          this.postsSubject.next([]);
-        }
-      );
-      return this.posts$;
-    } else {
-      this.http.get<any>(`${this.apiUrl}/posts/?limit=${limit}&offset=${offset}`, {withCredentials: true}).subscribe(
-        (data) => {
+  getPosts( limit: number = 20, offset: number = 0) {
+      this.http.get<any>(`${this.apiUrl}/posts/?limit=${limit}&offset=${offset}`, {withCredentials: true}).pipe(
+        tap((data) => {
           if (Array.isArray(data.posts)) {
             this.postsSubject.next(data.posts);
+            console.log("Posts obtenidos usuario:", this.postsSubject.value);
           } else {
             console.error("Error: La API no devolvió un array", data);
             this.postsSubject.next([]);
           }
-        },
-        (error) => {
+        }),
+        catchError((error) => {
           console.error("Error al obtener posts:", error);
           this.postsSubject.next([]);
-        }
-      );
+          return of(null); 
+        })
+      ).subscribe();
+      
       return this.posts$;
-    }
-    
   }
+
+  getPostsByUserId(userId: number) {
+    this.http.get<any>(`${this.apiUrl}/posts/user/${userId}`, { withCredentials: true }).pipe(
+      tap((data) => {
+        if (Array.isArray(data.posts)) {
+          this.postsUserSubject.next(data.posts);
+        } else {
+          console.error("Error: La API no devolvió un array", data);
+          this.postsUserSubject.next([]);
+        }
+      }),
+      catchError((error) => {
+        console.error("Error al obtener posts:", error);
+        this.postsUserSubject.next([]);
+        return of(null); 
+      })
+    ).subscribe(); 
+  
+    return this.postsUser$;
+  }
+
+  getSavedPosts() {
+    this.http.get<any>(`${this.apiUrl}/posts/savedposts`, {withCredentials: true}).pipe(
+      tap((data) => {
+        if (Array.isArray(data.posts)) {
+          if (data.posts.length === 0) {
+            this.postsSavedSubject.next([]); 
+          } else {
+            this.postsSavedSubject.next(data.posts); 
+          }
+        } else {
+          console.error("Error: La API no devolvió un array", data);
+          this.postsSavedSubject.next([]); 
+        }
+      }),
+      catchError((error) => {
+        console.error("Error al obtener posts:", error);
+        this.postsSavedSubject.next([]);
+        return of(null);
+      })
+    ).subscribe();
+    
+    return this.postsSaved$;
+  }
+  
 
   savePost(post: any) {
     this.http.post<any>(`${this.apiUrl}/posts/`, post, {withCredentials: true}).subscribe();
